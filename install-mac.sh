47e53ab63fce93f49454a41da7645d29a2532db4
Kevin Krauss <earlkrauss@gmail.com>
2026-02-11 12:22:19 -0800

    Update install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -3,13 +3,13 @@
 # Check for Homebrew and install if we don't have it
 if ! [ -x "$(command -v brew)" ]; then
   xcode-select --install
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   ln -s $HOME/.dotfiles/git/gitconfig.work $HOME/.gitconfig || echo ".gitconfig exists"
   sudo ln -s $HOME/.dotfiles/newsyslog.d/development.conf /etc/newsyslog.d/development.conf
 fi
 
 brew update
-brew bundle
+brew bundle --cleanup --no-lock
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
